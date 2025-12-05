49972217dc043b3f6fb2c225efd0294037ba6178
jkbworks <92801770+jkbworks@users.noreply.github.com>
2025-12-05 11:28:48 +0000

    Füge Teilnehmerbezeichnung hinzu und style sie

--- src/static/app.js
+++ src/static/app.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
-          <p><strong>Participants:</strong></p>
+          <p class="participants-label">Participants:</p>
           <ul>
             ${details.participants.map(participant => `<li>${participant}</li>`).join('')}
           </ul>
